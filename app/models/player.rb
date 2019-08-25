08d93571d5658e2cccb3de14dff17197a4186893
nbaugh1 <nbaugh1@gmail.com>
2019-08-25 01:25:15 -0400

    added pop out colapible list to team show page, pop out includes player weekly projections

--- app/models/player.rb
+++ app/models/player.rb
@@ -7,15 +7,13 @@ class Player < ActiveRecord::Base
 
   BASE_URL = "https://www.fantasysharks.com/players/playerpage.php?PID="
 
-  attr_accessor :profile_page_doc, :data
+  attr_accessor :profile_page_doc, :data, :projection
 
   def scrape_profile
     Nokogiri::HTML(open("#{BASE_URL}#{self.csv_id}"))
   end
 
-  def weekly_projections
-    json_file = File.read("db/seeds/weekly_data.json")
-    json_array = JSON.parse(json_file, :symbolize_names => true).flatten
-    @data = json_array
+  def projections
+    WeeklyProjection.find_by(csv_id: self.csv_id)
   end
 end
